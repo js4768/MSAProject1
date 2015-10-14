49fa64dce11b1fa72deb37937c06b755ac5c632f
js4768 <js4768@columbia.edu>
2015-10-13 21:40:41 -0400

    Three empty services running independently

--- build-system.sh
+++ build-system.sh
@@ -5,3 +5,6 @@ cd ../course-svc
 docker build -t course-svc .
 cd ../student-svc
 docker build -t student-svc .
+docker run -d -p 41000:3000 router
+docker run -d -p 42000:3000 course-svc
+docker run -d -p 43000:3000 course-svc
