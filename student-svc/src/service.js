d28b8fb38f70f24003e0714444e4e06286c19a12
js4768 <js4768@columbia.edu>
2015-10-14 22:21:10 -0400

    student service REST APIs and new Docker file

--- student-svc/src/service.js
+++ student-svc/src/service.js
@@ -1,7 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-app.get('/', function (req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/course', function (req, res) {
   res.send('This is student service');
+});
+
+app.post('/course/add', function (req, res) {
+  var title = req.body.title,
+      room = req.body.room;
+  res.send("A new course "+title+" will take place in Room "+room);
+});
+
+app.get('/course/info', function (req, res) {
+  var title = req.query['title'];
+  res.send("Something about course "+title);
+});
+
+app.delete('/course/delete', function (req, res) {
+  res.send("Delete a course "+req.body.title);
+});
+
+app.post('/course/addStudent', function (req, res) {
+  var studentID = req.body.SID,
+      courseID = req.body.CID;
+  res.send('A student '+studentID+' is added to course '+courseID);
+});
+
+app.post('/course/update', function (req, res) {
+  var title = req.body.title,
+      room = req.body.room,
+      prof = req.body.prof,
+      time = req.body.time;
+  if(prof == null) console.log("no prof");
+  res.send('Title: '+title+' Room: '+room+' Prof: '+prof+' Time: '+time);
 });
 
 var server = app.listen(3000, function () {
