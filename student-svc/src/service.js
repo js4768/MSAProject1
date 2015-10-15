39e1a966ee44c4ca75cef6ef0a6a3acce488e796
js4768 <js4768@columbia.edu>
2015-10-15 18:51:47 -0400

    Fixed name errors

--- student-svc/src/service.js
+++ student-svc/src/service.js
@@ -1,34 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017');
+
+var Student = mongoose.model('Student', {firstname: String, lastname: String});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.get('/course', function (req, res) {
+app.get('/student', function (req, res) {
   res.send('This is student service');
 });
 
-app.post('/course/add', function (req, res) {
-  var title = req.body.title,
-      room = req.body.room;
-  res.send("A new course "+title+" will take place in Room "+room);
+app.post('/student/add', function (req, res) {
+  var firstname = req.body.firstname,
+      lastname = req.body.lastname;
+  res.send("A new student is added to database. Firstname: "+firstname+" Lastname: "+lastname);
 });
 
-app.get('/course/info', function (req, res) {
+app.get('/student/info', function (req, res) {
   var title = req.query['title'];
-  res.send("Something about course "+title);
+  res.send("Something about student "+title);
 });
 
-app.delete('/course/delete', function (req, res) {
-  res.send("Delete a course "+req.body.title);
+app.delete('/student/delete', function (req, res) {
+  res.send("Delete a student "+req.body.title);
 });
 
-app.post('/course/addStudent', function (req, res) {
-  var studentID = req.body.SID,
-      courseID = req.body.CID;
-  res.send('A student '+studentID+' is added to course '+courseID);
-});
-
-app.post('/course/update', function (req, res) {
+app.post('/student/update', function (req, res) {
   var title = req.body.title,
       room = req.body.room,
       prof = req.body.prof,
@@ -41,5 +40,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Course service listening at http://%s:%s', host, port);
+  console.log('Student service listening at http://%s:%s', host, port);
 });
