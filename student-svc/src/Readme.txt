419263bb9e9b15c96fc9562bcde3a02dc54c87cc
js4768 <js4768@columbia.edu>
2015-10-16 18:04:49 -0400

    CRUD API completed with database integration

--- student-svc/src/Readme.txt
+++ student-svc/src/Readme.txt
@@ -1,3 +1,27 @@
 To compile service.js, run "npm install" first.
 
-config.xml contains mongodb ip:port and the schema used in the db.
+config.xml contains mongodb ip:port. Schema.son contains the schema used in the db.
+
+API:
+/student/add POST
+	When this API is called, a new user with the specified name will be created in the database. Returns a unique 6-digit ID after the add operation is successful.
+
+	Must be followed by a JSON request body. In the body a valid firstname and lastname field must be specified.
+
+/student/info GET
+	This API returns all student who satisfy certain conditions.
+
+	Use URL parameters to construct the query. If no parameter is present, the API returns all students in the database.
+
+/student/delete DELETE
+	This API deletes one student with the given student ID.
+
+	The request body must contain a valid student ID and only one student ID, nothing else is reported as an error.
+
+/student/update POST
+	This API allows user to update any field of a student (except for student ID).
+
+	All fields must be included in the database’s schema. If not, return 400. Must provide a valid student ID.
+
+/student/getall GET
+	This API returns all student info. No parameter needed.
--- student-svc/src/service.js
+++ student-svc/src/service.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+var mongoose_gen = require('mongoose-gen');
 var id_generator = require('./id_generator');
 var xml2js = require('xml2js');
 var util = require('util');
@@ -16,10 +17,13 @@ fs.readFile(__dirname + '/config.xml', function(err, data) {
       console.log("Connected to mongodb: "+mongodb_ip);
     });
 });
-
-var Student = mongoose.model('Student', {firstname: String, 
-                                         lastname: String, 
-                                         id: String});
+var studentJSON = JSON.parse(fs.readFileSync('schema.json', 'utf8'));
+var studentSchema = new mongoose.Schema(mongoose_gen.convert(studentJSON));
+var schemaList = [];  // a list to store all the schema in memory
+for(var name in studentJSON) {
+  schemaList.push(name);
+}
+var Student = mongoose.model('Student', studentSchema);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -29,10 +33,12 @@ app.get('/student', function (req, res) {
 
 app.post('/student/add', function (req, res) {
   if(req.body == null) {
-    res.status(500).send('Request body is empty!');
+    res.status(400).send('Request body is empty!');
+    return;
   }
   if(req.body.firstname == null || req.body.lastname == null) {
-    res.status(500).send('Bad request data!');
+    res.status(400).send('Firstname and lastname must be valid');
+    return;
   }
   var firstname = req.body.firstname,
       lastname = req.body.lastname,
@@ -42,6 +48,7 @@ app.post('/student/add', function (req, res) {
     if(err) {
       console.log("Saving to db failed. Student: "+firstname+" "+lastname);
       res.send("Failed to save student to database.");
+      return;
     }
   });
   res.send("A new student is added to database. Firstname: "+firstname+" Lastname: "+lastname+" ID: "+id);
@@ -49,54 +56,103 @@ app.post('/student/add', function (req, res) {
 
 app.get('/student/info', function (req, res) {
   if(req.query == null) {
-    res.status(500).send('Request body is empty!');
-  }
-  if(req.query['id'] == null) {
-    res.status(500).send('Bad request data!');
+    res.status(400).send('Request body is empty!');
+    return;
   }
   // List all attribute to find non-existing ones
   for(var name in req.query) {
-    console.log(name+"="+req.query[name]);
+    var valid = false;
+    for(var attribute in schemaList) {
+      if(name == schemaList[attribute]) {
+        valid = true;
+        break;
+      }
+    }
+    if(!valid) {
+      console.error("/student/info received incorrect field");
+      res.status(400).send("Bad request format");
+      return;
+    }
   }
-  var firstname = req.query["firstname"],
-      lastname = req.query["lastname"],
-      id = req.query["id"];
-  
-  Student.find({id:id}, function (err, result) {
+  var qry = {};
+  for(var param in req.query) {
+    qry[param] = req.query[param];
+  }
+  Student.find(qry, function (err, result) {
     if(err) {
       console.error("/student/info failed with student id "+id);
-      res.status(400).send("Internal errors");
+      res.status(500).send("Internal errors");
+      return;
     }
-    res.send("Something about student "+id+" "+result);
+    res.send(result);
   });
 });
 
 app.delete('/student/delete', function (req, res) {
   if(req.body == null) {
-    res.status(500).send('Request body is empty!');
+    res.status(400).send('Request body is empty!');
+    return;
   }
   if(req.body.id == null) {
-    res.status(500).send('Bad request data!');
+    res.status(400).send('Must provide student ID');
+    return;
   }
   var id = req.body.id;
+  if(id.length != 6) {
+    res.status(400).send('Invalid student ID');
+    return;
+  }
   Student.remove({id:id}, function (err) {
     if (err) {
       console.error("/student/delete failed with student id "+id);
-      res.status(400).send("Internal errors");
+      res.status(500).send("Internal errors");
+      return;
     }
   });
-  res.send("Delete a student "+req.body.id);
+  res.send("Delete student "+id);
 });
 
 app.post('/student/update', function (req, res) {
   if(req.body == null) {
-    res.status(500).send('Request body is empty!');
+    res.status(400).send('Request body is empty!');
+    return;
   }
   if(req.body.id == null) {
-    res.status(500).send('Bad request data!');
+    res.status(400).send('Must provide a student ID');
+    return;
   }
-  var id = req.body.id;
-  res.send("Info about student "+id);
+  // Screen input fields
+  for(var name in req.body) {
+    var valid = false;
+    for(var attribute in schemaList) {
+      if(name == schemaList[attribute]) {
+        valid = true;
+        break;
+      }
+    }
+    if(!valid) {
+      console.error("/student/update received incorrect field");
+      res.status(400).send("Bad request format");
+      return;
+    }
+  }
+  var update = {};
+  var qry = {id: req.body.id};
+  for(var param in req.body) {
+    if(param!="id")
+      update[param] = req.body[param];
+  }
+  var options = { multi: true };
+  Student.update(qry, update, options, function (err, numAffected) {
+    if(err || numAffected==0) {
+      console.error("/student/update cannot update student "+req.body.id);
+      res.status(500).send("Internal errors");
+      return;
+    }
+  });
+  Student.find(qry, function (err, result) {
+    res.send("Student update: "+result);
+  });
 });
 
 app.get('/student/getall', function (req, res) {
